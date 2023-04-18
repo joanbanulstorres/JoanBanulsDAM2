e571e3bdc7649e992f25783233a8afa7a2e4e91d
joanbanulstorres <joanbanyulstorres@gmail.com>
2023-04-18 16:16:32 +0200

    FCT - D13

--- FCT/procesaimgs/procesaimgs-opencv.py
+++ FCT/procesaimgs/procesaimgs-opencv.py
@@ -1,8 +1,15 @@
 import ctypes
+import cv2
 import os
+import random
+import time
 import tkinter as tk
+from datetime import datetime
+from PIL import Image
 from tkinter import filedialog
 from tkinter import ttk
+
+########## ▼ DECLARACIÓN DE FUNCIONES ▼ ##########
 
 def raton_dentro(e):
     e.widget['background'] = '#f2f2f2'
@@ -10,6 +17,7 @@ def raton_dentro(e):
 def raton_fuera(e):
     e.widget['background'] = '#ffffff'
 
+# Pide al usuario seleccionar un directorio (origen) y comprueba que no esté vacío y contenga imágenes en formato 'jpg'; si no es el caso, muestra los errores pertinentes
 def seleccionar_directorio_origen():
     directorio = filedialog.askdirectory()
     if directorio != "":
@@ -23,31 +31,69 @@ def seleccionar_directorio_origen():
                 extension_archvio = dividir_nombre[1]
                 if extension_archvio == ".jpg":
                     imagen = True
-                    print(elemento.name)
             if imagen == True:
-                ruta_origen.configure(bg=color_fondo)
+                ruta_origen.configure(bg=color_correcto)
                 error_directorio_origen.configure(text="")
             else:
                 ruta_origen.configure(bg=color_error)
                 error_directorio_origen.configure(text="*No contine imágenes con los formatos soportados")
         ruta_origen.configure(text=directorio)
+    habilitar_btn()
+    estado.configure(text="")
 
+# Pide al usuario seleccionar un directorio (destino)
 def seleccionar_directorio_destino():
     directorio = filedialog.askdirectory()
-    ruta_destino.configure(text=directorio)
+    if directorio != "":
+        ruta_destino.configure(text=directorio)
+        ruta_destino.configure(bg=color_correcto)
+    habilitar_btn()
+    estado.configure(text="")
 
+# Habilita el botón de generación de imágenes si los directorios de origen y destino están correctamente seleccionados
+def habilitar_btn():
+    if ruta_origen.cget("text") != "" and error_directorio_origen.cget("text") == "":       # Si el directorio origen está bien definido   
+        if ruta_destino.cget("text") != "" and error_directorio_destino.cget("text") == "": # Si el directorio destino está bien definido
+           btn_generar_imgs["state"] = "normal" 
+    else:
+        btn_generar_imgs["state"] = "disabled"
+            
+# Aplica un filtro de color aleatorio a unas imágenes dadas y las guarda en un directorio 
 def generar_imgs():
     print("Generando imagenes...")
-                    
+    contador = 1
+    for elemento in os.scandir(ruta_origen.cget("text")):
+        dividir_nombre = os.path.splitext(elemento.name)
+        extension_archvio = dividir_nombre[1]
+        if extension_archvio == ".jpg":
+            ruta_img0 = elemento.path.replace(os.sep, '/')                               # Para cambiar los back-slashes por forward-slashes
+            ruta_img = r'C:\Users\joan\Desktop\imgs2'
+            imagen = cv2.imread(ruta_img, 1)
+            imagen_modificada = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+            
+            nombre_img = "img" + str(contador) + " " + datetime.now().strftime('%Y-%m-%d %H-%M-%S') + ".jpg"
+            ruta = ruta_destino.cget("text") + "/" + nombre_img
+            cv2.imwrite(nombre_img, ruta)
+            contador += 1
+    estado.configure(text="Completado")
+##    time.sleep(5)
+##    estado.configure(text="")
+
+    
+########## ▲ DECLARACIÓN DE FUNCIONES ▲ ##########GENERALES ▼ ##########
+    
 ctypes.windll.shcore.SetProcessDpiAwareness(2)
 user32 = ctypes.windll.user32
 anchura = user32.GetSystemMetrics(0)*0.6
-altura = user32.GetSystemMetrics(1)*0.19
+altura = user32.GetSystemMetrics(1)*0.185
 
 color_fondo = '#ffffff'
 color_texto = '#000000'
-color_error = '#ff4848'
+color_correcto = '#90ff90'
+color_error = '#ff6860'
 mifuente = 'Verdana'
+
+########## ▲ VARIABLES GENERALES ▲ ##########
 
 
 ########## ▼ INTERFAZ DE USUARIO ▼ ##########
@@ -57,6 +103,7 @@ mifuente = 'Verdana'
 raiz = tk.Tk()
 raiz.title("Procesador de imágenes")
 raiz.geometry('%dx%d' % (anchura, altura))
+raiz.resizable(0, 0)
 raiz.configure(bg=color_fondo)
 estilo = ttk.Style()
 
@@ -70,7 +117,7 @@ texto_directorio_origen = tk.Label(raiz, text="Directorio origen:", font=(mifuen
 texto_directorio_origen.grid(sticky="w", row=0, column=0, padx=(15,0), pady=10)
 
 btn_directorio_origen = tk.Button(raiz, image=icono_directorio, bg=color_fondo, borderwidth=0, command=lambda:seleccionar_directorio_origen())
-btn_directorio_origen.grid(sticky="w", row=0, column=1, padx=(10,5), pady=10)
+btn_directorio_origen.grid(sticky="w", row=0, column=1, padx=5, pady=10)
 btn_directorio_origen.bind('<Enter>', raton_dentro)
 btn_directorio_origen.bind('<Leave>', raton_fuera)
 
@@ -89,7 +136,7 @@ texto_directorio_destino = tk.Label(raiz, text="Directorio destino:", font=(mifu
 texto_directorio_destino.grid(sticky="w", row=1, column=0, padx=(15,0), pady=(15,10))
 
 btn_directorio_destino = tk.Button(raiz, image=icono_directorio, bg=color_fondo, borderwidth=0, command=lambda:seleccionar_directorio_destino())
-btn_directorio_destino.grid(sticky="w", row=1, column=1, padx=(10,5), pady=10)
+btn_directorio_destino.grid(sticky="w", row=1, column=1, padx=5, pady=10)
 btn_directorio_destino.bind('<Enter>', raton_dentro)
 btn_directorio_destino.bind('<Leave>', raton_fuera)
 
@@ -106,7 +153,14 @@ formatos.grid(sticky="w", row=2, column=0, columnspan=4, padx=(15,0), pady=(5,10
 
 estilo.configure('personalizado.TButton', font=(mifuente, 10))
 btn_generar_imgs = ttk.Button(raiz, text="Generar imágenes", style='personalizado.TButton', command=lambda:generar_imgs())
-btn_generar_imgs.grid(sticky="w", row=3, column=0, columnspan=4, padx=(15,0), pady=(5,10))
+btn_generar_imgs.grid(sticky="w", row=3, column=0, padx=(15,0), pady=(5,10))
+btn_generar_imgs["state"] = "disabled"
+
+error_generar_imgs = tk.Label(raiz, text="", font=(mifuente,9), bg=color_fondo, fg=color_error)
+error_generar_imgs.grid(sticky="w", row=3, column=1, padx=(15,0), pady=(5,10))
+
+estado = tk.Label(raiz, text="", font=(mifuente,9), bg=color_fondo, fg=color_correcto)
+estado.grid(sticky="w", row=3, column=1, columnspan=3, padx=5, pady=(5,10))
 
 ########## ▼ ANTIALIASING TKINTER ▼ ##########
 
